2d2cdb12b83808f0540b0b5b1480cc0ecfe7effe
GITHUB Nexend <GITHUB ddante@ukr.net>
2020-12-23 17:52:57 +0300

    tasks

--- lesson21-dom/set-child-element/index.js
+++ lesson21-dom/set-child-element/index.js
@@ -1,4 +1,4 @@
-function setButton(buttonText) {
+export function setButton(buttonText) {
   const bodyElem = document.querySelector('body');
   bodyElem.innerHTML = `<button>${buttonText}</button>`;
 }
