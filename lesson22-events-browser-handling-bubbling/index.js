59390b31a56fc65a9dfaf3f1256c218b5f1d1c6a
GITHUB Nexend <GITHUB ddante@ukr.net>
2020-12-24 14:23:37 +0300

    fix

--- lesson22-events-browser-handling-bubbling/index.js
+++ lesson22-events-browser-handling-bubbling/index.js
@@ -5,12 +5,6 @@ const spanElem = document.querySelector('.rect_span');
 const eventsListElement = document.querySelector('.events-list');
 eventsListElement.textContent = '';
 
-const clearButton = document.querySelector('.clear-btn');
-function clearField() {
-  eventsListElement.innerHTML = '';
-}
-clearButton.addEventListener('click', clearField);
-
 const logTarget = (text, color) => {
   eventsListElement.innerHTML += `<span style="color: ${color}; margin-left: 8px;">${text}</span>`;
 };
@@ -23,32 +17,32 @@ const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
-divElem.addEventListener('click', logGreyDiv, true);
-pElem.addEventListener('click', logGreyP, true);
-spanElem.addEventListener('click', logGreySpan, true);
-
-divElem.addEventListener('click', logGreenDiv);
-pElem.addEventListener('click', logGreenP);
-spanElem.addEventListener('click', logGreenSpan);
-
-divElem.removeEventListener('click', logGreyDiv, true);
-pElem.removeEventListener('click', logGreyP, true);
-spanElem.removeEventListener('click', logGreySpan, true);
-
-divElem.removeEventListener('click', logGreenDiv);
-pElem.removeEventListener('click', logGreenP);
-spanElem.removeEventListener('click', logGreenSpan);
-
-const addEvents = document.querySelector('.attach-handlers-btn');
-function addAllEvents() {
-  logGreenDiv();
-  logGreenP();
-  logGreenSpan();
-  logGreySpan();
-  logGreyP();
-  logGreyDiv();
+const clearButton = document.querySelector('.clear-btn');
+function clearField() {
+  eventsListElement.innerHTML = '';
 }
-addEvents.addEventListener('click', addAllEvents, true);
+clearButton.addEventListener('click', clearField);
 
-const removeEvents = document.querySelector('.remove-handlers-btn');
-removeEvents.removeEventListener('click', addAllEvents, true);
+const attachHandlers = document.querySelector('.attach-handlers-btn');
+const addEvents = () => {
+  divElem.addEventListener('click', logGreyDiv, true);
+  pElem.addEventListener('click', logGreyP, true);
+  spanElem.addEventListener('click', logGreySpan, true);
+
+  spanElem.addEventListener('click', logGreenSpan);
+  pElem.addEventListener('click', logGreenP);
+  divElem.addEventListener('click', logGreenDiv);
+};
+attachHandlers.addEventListener('click', addEvents);
+
+const removeHandleds = document.querySelector('.remove-handlers-btn');
+const removeEvents = () => {
+  divElem.removeEventListener('click', logGreyDiv, true);
+  pElem.removeEventListener('click', logGreyP, true);
+  spanElem.removeEventListener('click', logGreySpan, true);
+
+  divElem.removeEventListener('click', logGreenDiv);
+  pElem.removeEventListener('click', logGreenP);
+  spanElem.removeEventListener('click', logGreenSpan);
+};
+removeHandleds.addEventListener('click', removeEvents);
